01558d720022abbaa12c6e7af71e00b9ccf43e6f
Lillian Luzinsky <eyeris@tpg.com.au>
2020-03-04 14:08:44 +1100

    trying to add progress scroll bar

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ const navSlide = () => {
 };
 
 navSlide();
+
+
+window.onscroll = function() {
+  this.progressBar();
+};
+
+function progressBar() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+progressBar();
